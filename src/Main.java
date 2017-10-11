6d5620b378cf2ef0cfa59ef3c57cc81a8b129528
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-10-12 00:29:50 +0700

    Create Collection of User

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 /**
  * Created by Nikita Zemlyanukhin on 11.10.2017.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        ArrayList<User> Users = new ArrayList<User>();
     }
 }
