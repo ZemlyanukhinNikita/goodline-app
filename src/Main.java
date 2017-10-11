0506b0fca5e7b8ea4377fbecbcec485a111dbb4f
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-10-12 00:08:32 +0700

    Adding new class User

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 /**
- * Created by dns on 11.10.2017.
+ * Created by Nikita Zemlyanukhin on 11.10.2017.
  */
 public class Main {
     public static void main(String[] args) {
---
+++ src/User.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Nikita Zemlyanukhin on 11.10.2017.
+ */
+public class User {
+    private Long id;
+    private String login;
+    private String password;
+
+    public User(Long id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
