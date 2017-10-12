6f48b6a75bd6e5a7f159a9394883cc02d6d05b0e
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-10-12 19:14:29 +0700

    Adding authentication

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,28 @@ public class Main {
         Users.add(new User((long)1,"Vasya","qwerty"));
         Users.add(new User((long)2,"Vasya123","123"));
 
+        if(args.length==2){
+            String login = args[0];
+            String password = args[1];
+            for(int i = 0; i < Users.size(); i++){
+                if((login.equals(Users.get(i).getLogin()) && password.equals(Users.get(i).getPassword()))){
+                    //System.out.println("0");}
+                    System.exit(0);}
+
+            }
+            for(int i = 0; i < Users.size(); i++){
+                if(args[0].equals(Users.get(i).getLogin())){
+                    if(args[1]!=(Users.get(i).getPassword())){
+                        System.exit(2);
+                    }}
+            }
+            for(int i = 0; i < Users.size(); i++){
+                if(args[0]!=(Users.get(i).getLogin())) {
+                    System.exit(1);
+                }
+            }
+        } else
+            System.out.println("Not enough data transmitted");
+
     }
 }
--- src/User.java
+++ src/User.java
@@ -36,4 +36,5 @@ public class User {
         this.password = password;
     }
 
+
 }
