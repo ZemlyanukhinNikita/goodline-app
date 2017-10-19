99c562803a96f62b80d37cb60c0e7f901cca4313
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-10-19 18:05:23 +0700

    some schanges and adding new tests in test.bat

--- src/Main.java
+++ src/Main.java
@@ -77,8 +77,8 @@ public class Main {
         ArrayList<ResourceUsersRoles> ResUserRoles = new ArrayList<ResourceUsersRoles>();
         ResUserRoles.add(new ResourceUsersRoles((long)1,(long)1,Roles.READ,"A.B"));
         ResUserRoles.add(new ResourceUsersRoles((long)2,(long)2,Roles.EXECUTE,"H.I.J"));
-        String login = args[0];
-        String password = args[1];
+        //String login = args[0];
+        //String password = args[1];
         //Authentication
         if (args.length < 2)  {
             System.out.println("Not enough data transmitted");
@@ -86,21 +86,21 @@ public class Main {
        if (args.length == 2){
 
             for(int i = 0; i < Users.size(); i++){
-                if((login.equals(Users.get(i).getLogin()) && password.equals(Users.get(i).getPassword()))){
+                if((args[0].equals(Users.get(i).getLogin()) && args[1].equals(Users.get(i).getPassword()))){
                     //System.out.println("0");}
                     System.exit(0);}
 
             }
-            isCorrectLoginPassword(login,password,Users);
+            isCorrectLoginPassword(args[0],args[1],Users);
 
         }
         boolean flag=false;
        if(args.length>=3) {
 
-            isCorrectLoginPassword(login,password,Users);
+            isCorrectLoginPassword(args[0],args[1],Users);
             for (int i = 0; i < Users.size(); i++) {
                 for (int j = 0; j < ResUserRoles.size(); j++) {
-                    if((Users.get(i).getId().equals(ResUserRoles.get(j).getUser_id())) && login.equals(Users.get(i).getLogin()) && password.equals(Users.get(i).getPassword())){
+                    if((Users.get(i).getId().equals(ResUserRoles.get(j).getUser_id())) && args[0].equals(Users.get(i).getLogin()) && args[1].equals(Users.get(i).getPassword())){
                        // System.out.println(Users.get(i).getId());
                        // System.out.println(ResUserRoles.get(j).getUser_id());
                       //  System.out.println(args[2]);
@@ -115,35 +115,43 @@ public class Main {
             }
             if(!flag){System.exit(3);}
         }
-        boolean flagRes=false;
-        boolean correctPath = false;
-        int k=0;
-        for (int j = 0; j <ResUserRoles.size() ; j++){
-        for (int i = 0; i < Users.size(); i++) {
+        if(args.length==4) {
+            boolean flagRes = false;
+            boolean correctPath = false;
+            int k = 0;
+            for (int j = 0; j < ResUserRoles.size(); j++) {
+                for (int i = 0; i < Users.size(); i++) {
 
-                if (login.equals(Users.get(i).getLogin()) && password.equals(Users.get(i).getPassword())) {
-                    if(Users.get(i).getId().equals(ResUserRoles.get(i).getUser_id())){
-                       // System.out.println(Users.get(i).getId());
-                       // System.out.println(ResUserRoles.get(j).getUser_id());
-                        if(args[3].equals(ResUserRoles.get(i).getPath()))
-                            flagRes=true;
-                            k=i;
+                    if (args[0].equals(Users.get(i).getLogin()) && args[1].equals(Users.get(i).getPassword())) {
+                        if (Users.get(i).getId().equals(ResUserRoles.get(i).getUser_id())) {
+                            // System.out.println(Users.get(i).getId());
+                            // System.out.println(ResUserRoles.get(j).getUser_id());
+                            if (args[3].equals(ResUserRoles.get(i).getPath()))
+                                flagRes = true;
+                            k = i;
+                        }
                     }
                 }
+
+            }
+            correctPath = isCorrectPath(args[3], ResUserRoles.get(k).getPath());
+            if (!correctPath && !flagRes) {
+                System.exit(4);
+            }
+        }
+       // System.out.println(args.length);
+        if(args.length==7) {
+            boolean validDateStart = isValidDate(args[4]);
+            boolean validDateEnd = isValidDate(args[5]);
+            if (!validDateStart || !validDateEnd) {
+                System.exit(5);
             }
 
+            boolean validVolume = isValidVolume(args[6]);
+            if (!validVolume) {
+                System.exit(5);
+            }
         }
-        correctPath = isCorrectPath(args[3],ResUserRoles.get(k).getPath());
-        if(!correctPath && !flagRes){System.exit(4);}
-
-        boolean validDateStart = isValidDate(args[4]);
-        boolean validDateEnd = isValidDate(args[5]);
-        if(!validDateStart || !validDateEnd) {System.exit(5);}
-
-        boolean validVolume = isValidVolume(args[6]);
-        if (!validVolume) {System.exit(5);}
-
-            
 
 
     }
