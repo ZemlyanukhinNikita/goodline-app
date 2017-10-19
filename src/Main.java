f058d562a1552903df5207ef9560e2a8b6847a9a
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-10-19 20:25:08 +0700

    Creating new methods isAuthentication, isAuthorization, isAccounts

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,7 @@ public class Main {
     public static boolean isValidVolume(String v) {
         try {
             int Volume = Integer.parseInt(v);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
         return true;
@@ -19,140 +18,134 @@ public class Main {
     public static boolean isValidDate(String d) {
         try {
             LocalDate data = LocalDate.parse(d);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
 
-    private static void isCorrectLoginPassword(String log,String pass,ArrayList<User> Users) {
+    private static void isAuthentication(String log, String pass, ArrayList<User> Users, int ArgLength) {
+        //System.out.println(ArgLenght);
+        if (ArgLength == 2) {
 
-        for(int i = 0; i < Users.size(); i++){
-            if(log.equals(Users.get(i).getLogin())){
-                if(!pass.equals(Users.get(i).getPassword())){
+            for (int i = 0; i < Users.size(); i++) {
+                if ((log.equals(Users.get(i).getLogin()) && pass.equals(Users.get(i).getPassword()))) {
+                    System.exit(0);
+                }
+
+            }
+
+        }
+        for (int i = 0; i < Users.size(); i++) {
+            if (log.equals(Users.get(i).getLogin())) {
+                if (!pass.equals(Users.get(i).getPassword())) {
                     System.exit(2);
-                }}
+                }
+            }
         }
         boolean l = false;
-        for(int i = 0; i < Users.size(); i++){
-            //System.out.println(Users.get(i).getLogin());
-           // System.out.println(log);
-            if(log.equals(Users.get(i).getLogin())){l=true;}
-            } if (!l) { System.exit(1);}
+        for (int i = 0; i < Users.size(); i++) {
+
+            if (log.equals(Users.get(i).getLogin())) {
+                l = true;
+            }
         }
+        if (!l) {
+            System.exit(1);
+        }
+    }
 
-        private static boolean isCorrectPath(String argPath, String path) {
-            String Arg = new String(argPath);
-            String Pth = new String(path);
-            String [] Arg1 = Arg.split("\\.");
-            String [] Pth1 = Pth.split("\\.");
-            if(Arg1.length<Pth1.length) {
-                return false;
-            } else {
-                for (int i = 0; i < Pth1.length; i++) {
-                    //System.out.println(Arg1[i]);
+    private static boolean isCorrectPath(String argPath, String path) {
+        String Arg = new String(argPath);
+        String Pth = new String(path);
+        String[] Arg1 = Arg.split("\\.");
+        String[] Pth1 = Pth.split("\\.");
+        if (Arg1.length < Pth1.length) {
+            return false;
+        } else {
+            for (int i = 0; i < Pth1.length; i++) {
+                if (!Arg1[i].equals(Pth1[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-                    //System.out.println(Str1[i]);
-                    //System.out.println("\\\\");
-                    if(!Arg1[i].equals(Pth1[i])){
-                        return false;
+    public static void isAuthorization(String log, String pass,String role,String resource, ArrayList<User> Users,ArrayList<ResourceUsersRoles> ResUserRoles, int ArgLength) {
+        isAuthentication(log,pass,Users,ArgLength);
+        boolean flag=false;
+        for (int i = 0; i < Users.size(); i++) {
+            for (int j = 0; j < ResUserRoles.size(); j++) {
+                if ((Users.get(i).getId().equals(ResUserRoles.get(j).getUser_id())) && log.equals(Users.get(i).getLogin()) && pass.equals(Users.get(i).getPassword())) {
+                    if (role.equals(ResUserRoles.get(j).getRole().toString())) {
+                        flag = true;
                     }
                 }
             }
-            return true;
-            // System.out.println(Arg1.length);
-            // System.out.println(Str1.length);
+        }
+        if (!flag) {
+            System.exit(3);
+        }
+        boolean flagRes = false;
+        boolean correctPath = false;
+        int k = 0;
+        for (int j = 0; j < ResUserRoles.size(); j++) {
+            for (int i = 0; i < Users.size(); i++) {
+                if (log.equals(Users.get(i).getLogin()) && pass.equals(Users.get(i).getPassword())) {
+                    if (Users.get(i).getId().equals(ResUserRoles.get(i).getUser_id())) {
+                        if (resource.equals(ResUserRoles.get(i).getPath()))
+                            flagRes = true;
+                            k = i;
+                    }
+                }
+            }
+        }
+        correctPath = isCorrectPath(resource, ResUserRoles.get(k).getPath());
+        if (!correctPath && !flagRes) {
+            System.exit(4);
+        } else {
+            System.exit(0);
+        }
+    }
 
-            // p=true;
-            //System.out.println(p);
+    public static void isAccounts(String sd, String ed, String vol){
+        boolean validDateStart = isValidDate(sd);
+        boolean validDateEnd = isValidDate(ed);
+        if (!validDateStart || !validDateEnd) {
+            System.exit(5);
+        }
+
+        boolean validVolume = isValidVolume(vol);
+        if (!validVolume) {
+            System.exit(5);
+        }
+
     }
 
     public static void main(String[] args) {
+        int ArgLenght = args.length;
         //Коллекция пользователей
         ArrayList<User> Users = new ArrayList<User>();
-        Users.add(new User((long)1,"Vasya","qwerty"));
-        Users.add(new User((long)2,"Vasya123","123"));
-
+        Users.add(new User((long) 1, "Vasya", "qwerty"));
+        Users.add(new User((long) 2, "Vasya123", "123"));
+        //Коллекция пользователей с ролями и ресурсами
         ArrayList<ResourceUsersRoles> ResUserRoles = new ArrayList<ResourceUsersRoles>();
-        ResUserRoles.add(new ResourceUsersRoles((long)1,(long)1,Roles.READ,"A.B"));
-        ResUserRoles.add(new ResourceUsersRoles((long)2,(long)2,Roles.EXECUTE,"H.I.J"));
-        //String login = args[0];
-        //String password = args[1];
-        //Authentication
-        if (args.length < 2)  {
+        ResUserRoles.add(new ResourceUsersRoles((long) 1, (long) 1, Roles.READ, "A.B"));
+        ResUserRoles.add(new ResourceUsersRoles((long) 2, (long) 2, Roles.EXECUTE, "H.I.J"));
+
+        if (args.length < 2) {
             System.out.println("Not enough data transmitted");
         }
-       if (args.length == 2){
-
-            for(int i = 0; i < Users.size(); i++){
-                if((args[0].equals(Users.get(i).getLogin()) && args[1].equals(Users.get(i).getPassword()))){
-                    //System.out.println("0");}
-                    System.exit(0);}
-
-            }
-            isCorrectLoginPassword(args[0],args[1],Users);
-
+        //Аутентификация
+        isAuthentication(args[0],args[1],Users,ArgLenght);
+        //Авторизация
+        if (args.length == 4) {
+        isAuthorization(args[0],args[1],args[2],args[3],Users,ResUserRoles,ArgLenght);
         }
-        boolean flag=false;
-       if(args.length>=3) {
-
-            isCorrectLoginPassword(args[0],args[1],Users);
-            for (int i = 0; i < Users.size(); i++) {
-                for (int j = 0; j < ResUserRoles.size(); j++) {
-                    if((Users.get(i).getId().equals(ResUserRoles.get(j).getUser_id())) && args[0].equals(Users.get(i).getLogin()) && args[1].equals(Users.get(i).getPassword())){
-                       // System.out.println(Users.get(i).getId());
-                       // System.out.println(ResUserRoles.get(j).getUser_id());
-                      //  System.out.println(args[2]);
-                       // System.out.println(ResUserRoles.get(j).getRole());
-                        if(args[2].equals(ResUserRoles.get(j).getRole().toString())){
-                            flag=true;
-                        }
-
-                    }
-                }
-
-            }
-            if(!flag){System.exit(3);}
+        //Аккаунтинг
+        if (args.length == 7) {
+            isAccounts(args[4],args[5],args[6]);
         }
-        if(args.length==4) {
-            boolean flagRes = false;
-            boolean correctPath = false;
-            int k = 0;
-            for (int j = 0; j < ResUserRoles.size(); j++) {
-                for (int i = 0; i < Users.size(); i++) {
-
-                    if (args[0].equals(Users.get(i).getLogin()) && args[1].equals(Users.get(i).getPassword())) {
-                        if (Users.get(i).getId().equals(ResUserRoles.get(i).getUser_id())) {
-                            // System.out.println(Users.get(i).getId());
-                            // System.out.println(ResUserRoles.get(j).getUser_id());
-                            if (args[3].equals(ResUserRoles.get(i).getPath()))
-                                flagRes = true;
-                            k = i;
-                        }
-                    }
-                }
-
-            }
-            correctPath = isCorrectPath(args[3], ResUserRoles.get(k).getPath());
-            if (!correctPath && !flagRes) {
-                System.exit(4);
-            }
-        }
-       // System.out.println(args.length);
-        if(args.length==7) {
-            boolean validDateStart = isValidDate(args[4]);
-            boolean validDateEnd = isValidDate(args[5]);
-            if (!validDateStart || !validDateEnd) {
-                System.exit(5);
-            }
-
-            boolean validVolume = isValidVolume(args[6]);
-            if (!validVolume) {
-                System.exit(5);
-            }
-        }
-
-
     }
 }
