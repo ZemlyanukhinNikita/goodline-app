eee8bb940e02ae1a9e4671792e2106e9e4419ed5
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-11-08 01:24:39 +0700

    adding new file BUILD.sh Roadmap2.md part 4

---
+++ BUILD.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf "out/classes"
+
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+	mkdir "out/classes"
+	CLASSES="out/classes/"
+	SRC="src"
+	CP="lib/*"
+	MAIN1="main.Main"
+fi
+
+find . -name "*.java" | xargs javac -cp "$CP" -d $CLASSES -sourcepath $SRC
+
+mkdir -p "out/lib/"
+cp $CP "out/lib/"
+cd out
+cd classes
+JAR="../Main.jar"
+CLASSES_JAR="./"
+jar -cfe "$JAR" "$MAIN1" $CLASSES_JAR
