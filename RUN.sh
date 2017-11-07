b75924a41541d71d7706f2ccfcb6059d7ca05455
ZemlyanukhinNikita <32156396+ZemlyanukhinNikita@users.noreply.github.com>
2017-11-08 01:43:30 +0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-	CP="out/lib/*;out/Main.jar;"
+	CP="out/lib/*;out/Main.jar"
+	MAIN2="main.Main"
+fi
+if [ "$(expr substr $(uname -s) 1 10)" == "Linux" ]; then
+	CP="out/lib/*:out/Main.jar"
 	MAIN2="main.Main"
 fi
 
