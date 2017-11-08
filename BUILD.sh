296957e28733736c604058b682ae40be62823d1e
ZemlyanukhinNikita <32156396+ZemlyanukhinNikita@users.noreply.github.com>
2017-11-08 19:55:38 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -3,20 +3,22 @@
 OUT="out/classes"
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-	mkdir -p "out/classes"
 	CLASSES="out/classes/"
 	SRC="src"
 	CP="lib/*"
 	MAIN1="main.Main"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	mkdir "out/classes"
 	CLASSES="out/classes/"
 	SRC="src"
 	CP="lib/*"
 	MAIN1="main.Main"
 fi
-find . -name "*.java" | xargs javac -cp "$CP" -d $CLASSES -sourcepath $SRC
+
+mkdir -p $OUT
 rm -rf "$OUT"
+
+find . -name "*.java" | xargs javac -cp "$CP" -d $CLASSES -sourcepath $SRC
+
 mkdir -p "out/lib/"
 cp $CP "out/lib/"
 cd out
