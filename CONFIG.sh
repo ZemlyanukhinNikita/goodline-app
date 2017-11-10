663304834ac1472b0673c1d82c0667dedef21348
ZemlyanukhinNikita <32156396+ZemlyanukhinNikita@users.noreply.github.com>
2017-11-10 10:45:58 +0700

    Update CONFIG.sh

--- CONFIG.sh
+++ CONFIG.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Путь до файлов .java
 SRC="src"
+# Путь до библиотек
 LIB="lib/*"
+# Главный класс приложения
 MAIN="main.Main"
+# Путь к библиотекам в out, лежащие рядом с .jar
 OUTLIB="out/lib/*"
+# Путь до .jar файла
 OUTJAR="out/Main.jar"
