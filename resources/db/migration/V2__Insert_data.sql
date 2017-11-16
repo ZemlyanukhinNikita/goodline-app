e2d95b0d1c51876a870b0f96ff3d3016eced3226
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-11-17 01:05:30 +0700

    change migration

--- resources/db/migration/V2__Insert_data.sql
+++ resources/db/migration/V2__Insert_data.sql
@@ -1,11 +1,11 @@
 INSERT INTO USER (ID,LOGIN,PASSWORD,SALT) VALUES
-  (1, 'Vasya', 'd8578edf8458ce6fbc5bb76a58c5ca4', '8169f7411b8f74150ad38d7d6b0435d'),
-  (2, 'Vasya123', '202cb962ac5975b964b7152d234b70', '67e88bbe316e44638deecf475daff9f8'),
-  (3, 'jdoe', 'e9c37b6b49be2d96f4664db72980321c', 'b1e26bb73d6fb25aae62b3fcc61cc7'),
-  (4, 'jrow', 'e79913a0dbb45bcd7b6723523d68c146', '130aecb58f9d1f8326ce4a5b2'),
-  (5, 'xxx', 'f0a458fd33489695d53df156b77c724', 'a989cbd96aaa7acdebdac36eb9cb6233');
+  (1, 'Vasya', '82d8b0e268ddc216d347bcb95b158d92', '8169f7411b8f74150ad38d7d6b0435d'),
+  (2, 'Vasya123', '27659c386f539b79b3344ff8351956', '67e88bbe316e44638deecf475daff9f8'),
+  (3, 'jdoe', 'f9f9e08b5f19658e76cb5d61dcdfc3c4', 'b1e26bb73d6fb25aae62b3fcc61cc7'),
+  (4, 'jrow', '11c3d296c2c971abc3510d385ce7c76', '130aecb58f9d1f8326ce4a5b2'),
+  (5, 'xxx', '66327f90935047794391d476e3fb4dd', 'a989cbd96aaa7acdebdac36eb9cb6233');
 
-INSERT INTO RESOURCE_USERS_ROLES VALUES
+INSERT INTO RESOURCE_USERS_ROLES (ID,USER_ID,ROLE,PATH) VALUES
   (1,1,'READ','A.B'),
   (2,1,'READ','H.I.J'),
   (3,1,'WRITE','H.I.J'),
