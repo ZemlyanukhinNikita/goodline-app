71983ad2c788a723516cbb8ab7128563f2c6ffe2
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-11-23 23:40:01 +0700

    create MyException class

---
+++ src/service/MyException.java
@@ -0,0 +1,20 @@
+package service;
+
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+
+    public MyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
