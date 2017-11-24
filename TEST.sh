cca117ceca02f400be810e866410c0a4fc8f002c
ZemlyanukhinNikita <32156396+ZemlyanukhinNikita@users.noreply.github.com>
2017-11-24 15:12:22 +0700

    Update TEST.sh

--- TEST.sh
+++ TEST.sh
@@ -64,8 +64,8 @@ test "-l Vasya -p qwerty -r WRIRE -pt A.B" 3
 test "-l Vasya123 -p 123 -r READ -pt DDD" 0
 
 #По скольку это последний тест, можем переопределить переменные окружения
-LOGIN="admin1"
-PASSWORD="db_h2_17"
+export LOGIN="admin1"
+export PASSWORD="db_h2_17"
 test "" 255
 
 if [[ $counter -eq 0 ]]; then
