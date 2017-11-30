7a76539bb10b3fd95ed71407ab40fb84e498647c
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-12-01 01:13:17 +0700

    adding dependency mockito and create class AaaServiceTest

---
+++ src/test/java/service/AaaServiceTest.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.junit.Test;
+
+public class AaaServiceTest {
+    @Test
+    public void authenticate() throws Exception {
+    }
+
+    @Test
+    public void authorize() throws Exception {
+    }
+
+    @Test
+    public void account() throws Exception {
+    }
+
+}
