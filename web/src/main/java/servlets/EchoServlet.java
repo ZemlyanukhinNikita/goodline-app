310e384e2735d61ddd1fbc6731e76d2f69540791
ZemlyanukhinNikita <nakeg8@gmail.com>
2017-12-16 01:37:11 +0700

    add inject logger

--- web/src/main/java/servlets/EchoServlet.java
+++ web/src/main/java/servlets/EchoServlet.java
@@ -1,5 +1,8 @@
 package servlets;
 
+import customInjections.InjectLogger;
+import org.apache.logging.log4j.core.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,14 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class EchoServlet extends HttpServlet {
+    @InjectLogger
+    Logger logger;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/index.jsp").forward(req,resp);
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+        logger.debug("doGet");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.sendRedirect("get?id=" + req.getParameter("id"));
+        logger.debug("doPost");
     }
 
 }
